c111e5b859c3873e0123f230b24dd0526ea2149e
unknown <santhaseelank2025@gmali.com>
2023-10-22 16:35:24 +0530

    first commit

--- Task_ Assets/app.js
+++ Task_ Assets/app.js
@@ -1,23 +1,22 @@
-function fetchData() {
-  fetch('https://jsonplaceholder.typicode.com/comments')
-      .then(response => response.json())
-      .then(data => {
-          const main = document.querySelector('main');
-          data.forEach(comment => {
-              const commentElement = document.createElement('div');
-              commentElement.classList.add('comment');
-              commentElement.innerHTML = `
-                  <h3>${comment.name}</h3>
-                  <p>Email: ${comment.email}</p>
-                  <p>${comment.body}</p>
-              `;
-              main.appendChild(commentElement);
-          });
-      })
-      .catch(error => {
-          console.error('Error fetching data:', error);
-      });
-}
+
+fetch('https://jsonplaceholder.typicode.com/comments')
+.then(res => res.json())
+.then(data => { 
+    data.forEach((user) => {
+        const commentSection = document.querySelector('.comment-section')
+        const commentBox = document.createElement('div')
+        commentBox.classList.add('comment-box')
+        commentBox.innerHTML = `Name: <span>${user.name}</span></div>
+                                <div class='email'>Email: <span>${user.email}</span></div>
+                                <div class='comment'>${user.body}</div>`
+        commentSection.appendChild(commentBox)
+
+    }) 
+})
+
+.catch(error => console.error('Error', error))
 
 
-fetchData();
+
+
+    
